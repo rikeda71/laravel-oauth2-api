e68f5bfe80fbc06bf2ebbfd29b75459f22eee067
Ryuya Ikeda <rikeda71@gmail.com>
2021-02-07 22:54:26 +0900

    update user migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,13 +14,18 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
+            $table->bigIncrements('id');
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->string('password')->nullable(); // nullable because using social login
             $table->rememberToken();
             $table->timestamps();
+
+            // use in auth
+            $table->string('provider_id')->nullable();
+            $table->string('provider_name')->nullable();
+            $table->unique(['provider_id', 'provider_name']);
         });
     }
 
