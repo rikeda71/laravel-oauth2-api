c64d7473c827ca713355ae3168a25f7d6792a0e5
Ryuya Ikeda <rikeda71@gmail.com>
2021-02-11 23:46:28 +0900

    add comment to oauth controller

--- app/Http/Controllers/OAuthController.php
+++ app/Http/Controllers/OAuthController.php
@@ -8,11 +8,13 @@ use Laravel\Socialite\Facades\Socialite;
 class OAuthController extends Controller
 {
     public function auth(string $provider) {
+        // 以下でリダイレクト先URLを取得できるので、これを返却するのもあり
+        // Socialite::driver($provider)->stateless()->redirect()->getTargetUrl();
         return Socialite::driver($provider)->redirect();
     }
 
     public function callback(string $provider) {
-        $user = Socialite::driver($provider)->user();
+        $user = Socialite::driver($provider)->stateless()->user();
         return $user->getEmail();
     }
 }
