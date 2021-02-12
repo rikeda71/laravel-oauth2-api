8a9bb032a5f817749f7c311d6f9722f88617e4c0
Ryuya Ikeda <rikeda71@gmail.com>
2021-02-12 20:25:53 +0900

    refactor user create service and add test

--- app/Http/Services/UserCreateService.php
+++ app/Http/Services/UserCreateService.php
@@ -2,36 +2,73 @@
 
 namespace App\Http\Services;
 
+use Throwable;
+
 use App\Models\SocialRelation;
 use App\Models\User;
+use Illuminate\Database\DatabaseManager;
 
 class UserCreateService
 {
-    public function __construct()
+    /**
+     * @var User
+     */
+    private $userRepository;
+
+    /**
+     * @var SocialRelation
+     */
+    private $socialRelationRepository;
+
+    /**
+     * @var DatabaseManager
+     */
+    private $dbm;
+
+    /**
+     * UserCreateService constructor.
+     * @param User $userRepository
+     * @param SocialRelation $socialRelationRepository
+     * @param DatabaseManager $dbm
+     */
+    public function __construct(User $userRepository, SocialRelation $socialRelationRepository, DatabaseManager $dbm)
     {
-        //
+        $this->userRepository = $userRepository;
+        $this->socialRelationRepository = $socialRelationRepository;
+        $this->dbm = $dbm;
     }
 
+    /**
+     * @param string $provider
+     * @param string $name
+     * @param string $email
+     * @param string $socialUserId
+     * @throws Throwable
+     */
     public function execute(string $provider, string $name, string $email, string $socialUserId): void
     {
+        $this->dbm->beginTransaction();
         try {
             // https://github.com/HeshamAdel007/MoviesApp/blob/master/back-end/app/Http/Controllers/Api/Auth/SocialiteLoginController.php
             // TODO: パスワードも用意できるようにしてみる
-            $appUser = User::create([
+            $appUser = $this->userRepository::create([
                 'name' => $name,
                 'email' => $email,
             ]);
             // ソーシャルログイン用のテーブルのマイグレーションを作ってみる
             $socialRelation = $appUser->socialLogin()->where('provider', $provider)->first();
-            if (!$socialRelation) {
-                SocialRelation::create([
+            if ($socialRelation != null) {
+                printf($socialRelation);
+                $this->socialRelationRepository::create([
                     'provider' => $provider,
                     'user_id' => $appUser->id,
                     'provider_user_id' => $socialUserId,
                 ]);
             }
+            $this->dbm->commit();
         } catch (\Exception $e) {
-            throw new \RuntimeException('failed to create application user. cause: ' . $e);
+            $this->dbm->rollback();
+            throw new \RuntimeException('failed to create application user.  cause: ' . $e);
         }
     }
 }
