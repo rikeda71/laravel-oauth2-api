caf9331f19590785086393e6a1459144de6a3f9b
Ryuya Ikeda <rikeda71@gmail.com>
2021-02-12 16:45:35 +0900

    make social relation migration and auth settings

---
+++ database/migrations/2021_02_11_070658_create_social_relation_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSocialRelationTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('social_relation', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('user_id');
+            $table->string('provider');
+            $table->string('provider_user_id');
+            // foreign key
+            $table->foreign('user_id')
+                ->references('id')->on('users')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('social_relation');
+    }
+}
