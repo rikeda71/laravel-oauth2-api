cc0045c43b18b8525d893ef21cac5d4700ed6fc3
Ryuya Ikeda <rikeda71@gmail.com>
2021-02-13 14:13:48 +0900

    refactor usecase and add test for usecase

--- app/Http/UseCases/OAuthUseCase.php
+++ app/Http/UseCases/OAuthUseCase.php
@@ -2,21 +2,34 @@
 
 namespace App\Http\UseCases;
 
-use App\Http\Services\UserCreateService;
 use App\Http\Services\UserLoginService;
+use Illuminate\Http\JsonResponse;
 
 class OAuthUseCase
 {
 
+    /**
+     * @var UserLoginService
+     */
     private $userLoginService;
 
-    public function __construct(UserLoginService $userLoginService, UserCreateService $userCreateService)
+    /**
+     * OAuthUseCase constructor.
+     * @param UserLoginService $userLoginService
+     */
+    public function __construct(UserLoginService $userLoginService)
     {
         $this->userLoginService = $userLoginService;
     }
 
-    public function execute(string $provider): \Illuminate\Http\JsonResponse
+    /**
+     * @param string $provider
+     * @return JsonResponse
+     * @throws \App\Exceptions\UserLoginException
+     * @throws \Throwable
+     */
+    public function execute(string $provider): JsonResponse
     {
-        return response()->json(['token' => $this->userLoginService->execute($provider)]);
+        return new JsonResponse(['token' => $this->userLoginService->execute($provider)]);
     }
 }
