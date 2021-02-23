c5745176589c1e7faec7d5a203288b09bd2653d8
Ryuya Ikeda <rikeda71@gmail.com>
2021-02-23 23:01:06 +0900

    refactor auth test

--- tests/Unit/Controllers/Auth/GoogleOAuthControllerTest.php
+++ tests/Unit/Controllers/Auth/GoogleOAuthControllerTest.php
@@ -3,41 +3,10 @@
 namespace Tests\Unit\Controllers\Auth;
 
 use Laravel\Socialite\Facades\Socialite;
-use Mockery;
-use Tests\TestCase;
 
-class GoogleOAuthControllerTest extends TestCase
+class GoogleOAuthControllerTest extends AbstractAuthControllerTest
 {
     const GoogleProvider = 'google';
-
-    /**
-     * @var Mockery\LegacyMockInterface|Mockery\MockInterface
-     */
-    private $userProvider;
-
-    public function setUp(): void
-    {
-        parent::setUp();
-
-        // ログインユーザのmock
-        $authUser = Mockery::mock('Laravel\Socialite\Two\User');
-        $authUser
-            ->shouldReceive('getId')
-            ->andReturn(uniqid())
-            ->shouldReceive('getEmail')
-            ->andReturn(uniqid().'@test.com')
-            ->shouldReceive('getNickName')
-            ->andReturn('Nick');
-
-        $this->userProvider = Mockery::mock('Laravel\Socialite\Constracts\Provider');
-        $this->userProvider->shouldReceive('user')->andReturn($authUser);
-    }
-
-    public static function tearDownAfterClass(): void
-    {
-        parent::tearDownAfterClass();
-        Mockery::getConfiguration()->allowMockingNonExistentMethods(true);
-    }
 
     public function testShowGoogleOAuthScreen(): void
     {
